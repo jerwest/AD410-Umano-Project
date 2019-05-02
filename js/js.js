a013d0afc402ebaacfd9a179470019efc410b00b
rbeeler <ruthbeeler3@gmail.com>
2019-05-01 17:27:24 -0700

    moved initMap() to js file

--- js/js.js
+++ js/js.js
@@ -1,3 +1,16 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 47.608013, lng: -122.335167},
+    zoom: 14
+  });
+  var marker = new google.maps.Marker({
+    position: {lat: 47.608013, lng: -122.335167},
+    map: map,
+    title: 'Hello World!'//Downtown Seattle
+  });
+}
+
 function trafficOverlay() {
   var trafficLayer = new google.maps.TrafficLayer();
   trafficLayer.setMap(map);
