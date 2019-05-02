a7ffe6b8e22d275cc976cebe016c9e02ddbd6821
rbeeler <ruthbeeler3@gmail.com>
2019-05-01 17:44:31 -0700

    included geocoding bar

--- js/js.js
+++ js/js.js
@@ -1,13 +1,28 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.608013, lng: -122.335167},
-    zoom: 14
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 47.6062, lng: -122.3321},
+    zoom: 12
   });
-  var marker = new google.maps.Marker({
-    position: {lat: 47.608013, lng: -122.335167},
-    map: map,
-    title: 'Hello World!'//Downtown Seattle
+
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
 
