16bbeb703efa6206f3ed1b90904c6068914a3533
Mustafa Elimam <mjkam88@gmail.com>
2019-05-25 19:54:50 -0700

    code/style

--- public/javascripts/index-script.js
+++ public/javascripts/index-script.js
@@ -2,6 +2,9 @@ var map
 var markers = []
 var trafficLayer = {}
 var bikeLayer = {}
+var styleControl = {}
+var styleSelector= {}
+
 
 // start out with filter features set to false, so no filtering happens by default
 var filters = {single_family:false, multi_family:false, condo_townhome:false, retail:false, office:false}
@@ -123,11 +126,107 @@ function initMap() {
       center: {lat: 47.700578, lng: -122.325019}
     }
 
-    map_document = document.getElementById('map')
-    map = new google.maps.Map(map_document,map_options);
     loadMarkers()
 
+    map_document = document.getElementById('map')
+    map = new google.maps.Map(map_document,map_options);
+
+    styleControl = document.getElementById('style-selector-control');
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(styleControl);
+
+    styleSelector = document.getElementById('style-selector');
+    map.setOptions({styles: styles[styleSelector.value]});
+
+    styleSelector.addEventListener('change', function() {
+    map.setOptions({styles: styles[styleSelector.value]});
+    });
+
 }
+
+
+var styles = {
+        default: null,
+        night: [
+          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{color: '#263c3f'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#6b9a76'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#38414e'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#212a37'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9ca5b3'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#746855'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#1f2835'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#f3d19c'}]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{color: '#2f3948'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#17263c'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#515c6d'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#17263c'}]
+          }
+        ],
+      };
 
 /* Overlays */
 function trafficToggle(){
