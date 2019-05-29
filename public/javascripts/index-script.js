fa8ae659887754da3bbd737f3eb16b924e67c7d6
rbeeler <ruthbeeler3@gmail.com>
2019-05-28 20:07:59 -0700

    added a "former" property(north seattle college for now) and made its marker black

--- public/javascripts/index-script.js
+++ public/javascripts/index-script.js
@@ -7,7 +7,7 @@ var styleSelector= {}
 
 
 // start out with filter features set to false, so no filtering happens by default
-var filters = {single_family:false, multi_family:false, condo_townhome:false, retail:false, office:false}
+var filters = {single_family:false, multi_family:false, condo_townhome:false, retail:false, office:false, former:false}
 // throwawayline
 $(function () {
     $('input[name=filter]').change(function (e) {
